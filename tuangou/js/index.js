107ea23ff14852924c5145ff72aff505adbfa427
quietchunYoung <quietchunyoung@gmail.com>
2017-02-19 23:53:27 +0800

    Add files via upload

---
+++ tuangou/js/index.js
@@ -0,0 +1,91 @@
+
+// function shoplist(){
+// 	for(var i=0;i<data.length;i++){
+// 		var shopPicture=data[i].shopPicture;
+// 		var shopName=data[i].shopName;
+// 		var price=data[i].price;
+// 		var address=data[i].address;
+// 		var type=data[i].type;
+// 		var isPromo=data[i].isPromo;
+// 		var isHui=data[i].isHui;
+// 		var group='';
+// 		var Hui='';
+		
+// 		if(isPromo){
+			
+// 			var group='group';
+// 			console.log(isPromo+'1');
+// 		}
+// 		if(isHui){
+			
+// 			var Hui='isHui';
+// 			console.log(isHui);
+// 		}
+
+
+
+// 		var star='';
+// 		switch (data[i].star) {
+//  			case 0: var star='star0';
+//     		break;
+//   			case 1: var star='star1';
+//     		break;
+//   			case 2: var star='star2';
+//     		break;
+//     		case 3: var star='star3';
+//     		break;
+//     		case 3.5: var star='star35';
+//     		break;
+//     		case 4: var star='star4';
+//     		break;
+//     		case 4.5: var star='star45';
+//     		break;
+//     		case 5: var star='star5';
+//     		break;
+    		
+  			
+// }
+      
+
+	
+
+// 		document.write('\
+// <section>\
+// 	<div class="big">\
+// 		<div class="img">\
+// 			<img src="'+shopPicture+'"/>\
+// 		</div>\
+// 		<div class="content">\
+// 			<h2><span class="shopName">'+shopName+'</span><span class="'+group+'"></span><span class="'+Hui+'"></span></h2>\
+// 			<p><span class="'+star+'"></span>￥<span class="price">'+price+'</span>/人</p>\
+// 			<p><span class="address">'+address+'</span><span class="cuisine">'+type+'</span></p>\
+// 		</div>\
+// 	</div>\
+// 		')
+
+// 	for(var j=0;j<data[i].tuan.length;j++){
+// 		 var tuanTitle=data[i].tuan[j].title;
+// 		 if(data[i].tuan[j].type==1){
+// 		 	var tuantype='tuan-type1';
+// 		 }
+// 		 else {
+// 		 	var tuantype='tuan-type2';
+// 		 }
+		 
+
+
+
+// 		document.write('<p class="shop-type">\
+// 		<span class="'+tuantype+'"></span>\
+// 		<span class="tuan-title">'+tuanTitle+'</span>\
+// 		<span class="tuan-arrow"></span>\
+// 	</p>\
+// 	')
+// 		}
+
+	
+// 	document.write('</section>\
+// 		')
+// 	}	
+// 	}
+// 	shoplist();
